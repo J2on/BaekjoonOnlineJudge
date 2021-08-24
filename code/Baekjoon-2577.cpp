a2d5a80fd06ce6f36bf6285d989cb05a1ff269f5
JohnJH <>
2021-08-24 14:15:01 +0000

    2577

---
+++ code/Baekjoon-2577.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int FindNum(const int &Result, const int &SelectNum){
+  int Count = 0;
+
+}
+
+int main() {
+  int NumA;
+  int NumB;
+  int NumC;
+  int Result;
+  
+  scanf("%d %d %d", NumA, NumB, NumC);
+  Result = NumA * NumB * NumC;
+  
+  
+}
+
+
