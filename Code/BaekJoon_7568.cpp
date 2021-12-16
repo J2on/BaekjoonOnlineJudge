9189cddafe994bdb3cc534c123132129ec29d0a7
전재현 <63008127+J2on@users.noreply.github.com>
2021-12-16 22:42:59 +0900

    나는나는 바보

--- Code/BaekJoon_7568.cpp
+++ Code/BaekJoon_7568.cpp
@@ -1,78 +1,38 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
 /*
   BaekJoon_7568 덩치
+  핵심을 잘못생각하고 있었다.
+  역시 이렇게 어려울리가 없었다
+  3중 벡터를 막 쓰고 했었는데
+  결론은 그냥
+  모두 입력받고
+  i번째 친구가 나머지 친구들과 비교해서 큰애가 있으면 등수를 하나 씩 더해주면 끝이었다
+  초등 올림피아에 나올정도면 초등생들도 해결하는 문제일건데
+  나는 왜 이렇게 멍청한지 모르겠다.
 */
 
 int main() {
   int numN;
-  cin >> numN;
-  vector<int> size(3);
+  scanf("%d", &numN);
+  vector<int> size(2);
   vector<vector<int>> sizeVec;
-  vector<vector<vector<int>>> sizeRank;
   for(int i=0; i<numN; i++){
     scanf("%d %d", &size[0], &size[1]);
-    size[2] = i;
     sizeVec.emplace_back(size);
-    sizeRank.emplace_back(sizeVec);
-    sizeVec.clear();
-  } // size는 몸무게 키 입력받은순서 를 저장함
-  
-  for(int i=0;i<numN; i++){
-    printf("%d %d %d\n", sizeRank[i][0][0], sizeRank[i][0][1], sizeRank[i][0][2]);
   }
-
-
-  bool isDone;
-  vector<vector<int>> temp;
-  while(1){
-    isDone = true;
-    for(int i=0; i<numN-1; i++){
-      if(sizeRank[i+1][0][0] > sizeRank[i][0][0]){ // 몸무게가 큰경우
-        if(sizeRank[i+1][0][1] >= sizeRank[i][0][1]){ // 키가 크거나 같은경우
-          temp = sizeRank[i];
-          sizeRank.erase(sizeRank.begin()+i);
-          sizeRank.emplace(sizeRank.begin()+i, temp);
-          isDone = false;
-        }
-        else { // 몸무게가 큰데 키가 작은경우 => 둘은 같은 등수를 가짐
-          for(int k=0; k<sizeRank[i+1].size(); k++){
-            sizeRank[i].emplace_back(sizeRank[i+1][k]);
-          }
-          sizeRank[i+1].clear();
-          isDone = false;
-        }
-      }   
-      else if(sizeRank[i+1][0][0] == sizeRank[i][0][0]){ // 몸무게가 같은경우
-        if(sizeRank[i+1][0][1] > sizeRank[i][0][1]){ // 키가 큰 경우
-          temp = sizeRank[i];
-          sizeRank.erase(sizeRank.begin()+i);
-          sizeRank.emplace(sizeRank.begin()+i, temp);
-          isDone = false;
-        }
-        else if(sizeRank[i+1][0][1] == sizeRank[i][0][1]){ // 키가 같은 경우
-          for(int k=0; k<sizeRank[i+1].size(); k++){
-            sizeRank[i].emplace_back(sizeRank[i+1][k]);
-          }
-          sizeRank[i+1].clear();
-          isDone = false;
-        } // 키가 작은 경우는 생각하지 않음
-      }
-      else { //몸무게가 작은 경우
-        if(sizeRank[i+1][0][1] > sizeRank[i][0][1]){ // 키가 큰 경우
-          for(int k=0; k<sizeRank[i+1].size(); k++){
-            sizeRank[i].emplace_back(sizeRank[i+1][k]);
-          }
-          sizeRank[i+1].clear();
-          isDone = false;
-        }
+  int rank;
+  
+  for(int i=0; i<numN; i++){
+    rank = 1;
+    for(int k=0; k<numN; k++){
+      if(sizeVec[k][0] > sizeVec[i][0] && sizeVec[k][1] > sizeVec[i][1]){
+        rank++;
       }
     }
-    if(isDone == true) break;
+    printf("%d ", rank);
   }
-
-  // 이렇게 풀다가 망함 ㅋ
-}  
+  
+}
