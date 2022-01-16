3f969584be2ab15c76e23e0906d473d4af5704fa
전재현 <63008127+J2on@users.noreply.github.com>
2022-01-16 23:54:28 +0900

    구현은 성공했지만 시간초과....

--- Code/BaekJoon_18870.cpp
+++ Code/BaekJoon_18870.cpp
@@ -9,27 +9,49 @@ using namespace std;
   가장 작은애 0 다음 1 다음 2 
   이런식으로 바꿔버리는거 
 */
+void swapE(int &numA, int &numB){
+  int numT = numA;
+  numA = numB;
+  numB = numT;
+}
 
 int main(){
   int numN;
   scanf("%d",&numN);
-  int *arrCount = new int[2000000001];
-  for(int i=0; i<2000000001; i++){
-    arrCount[i] = 0;
+  
+  vector<int> numVec;
+  int numX;
+  for(int i=0; i<numN; i++){
+    cin>> numX;
+    numVec.push_back(numX);
+  }
+
+  vector<int> copyVec;
+  for(int i=0; i<numN; i++){
+    copyVec.push_back(numVec[i]);
+  }
+  
+  bool isDone;
+  while(1){
+    isDone = true;
+    for(int i=0; i<numVec.size()-1; i++){
+      if(numVec[i] > numVec[i+1]){
+        swapE(numVec[i], numVec[i+1]);
+        isDone = false;
+      }
+      else if(numVec[i] == numVec[i+1]){
+        numVec.erase(numVec.begin()+i);
+        i--;
+      }
+    }
+    if(isDone == true) break;
   }
 
   for(int i=0; i<numN; i++){
-    arrCount[i+1000000000]++;
-  }
-
-  vector<int> vecResult;
-  for(int i=0; i<2000000001; i++){
-    if(arrCount[i] != 0){
-      vecResult.push_back(i-1000000000);
+    for(int j=0; j<numVec.size(); j++){
+      if(copyVec[i] == numVec[j]){
+        printf("%d ", j);
+      }
     }
-  }
-
-  for(int i:vecResult){
-    cout << i << endl;
   }
 }
