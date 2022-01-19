a08aad856b3fc554b444c45adb386fe4a935b493
전재현 <63008127+J2on@users.noreply.github.com>
2022-01-19 23:01:04 +0900

    집에 가고 싶습니다

--- Code/BaekJoon_18870.cpp
+++ Code/BaekJoon_18870.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
-
 using namespace std;
 
 /*
@@ -9,42 +8,31 @@ using namespace std;
   도대체 이게 무슨 소리일까 했는데
   리스트 안에 들어있는 수들을 쫙 줄세워서
   가장 작은애 0 다음 1 다음 2 
-  이런식으로 바꿔버리는거 0.000552
+  이런식으로 바꿔버리는거 
 */
-void swapE(int &numA, int &numB){
-  int numT = numA;
-  numA = numB;
-  numB = numT;
-}
 
 int main(){
-  
-
   int numN;
   scanf("%d",&numN);
   
   vector<int> numVec;
+  int copyArr[numN];
   int numX;
   for(int i=0; i<numN; i++){
     cin>> numX;
     numVec.push_back(numX);
+    copyArr[i] = numX;
   }
 
-  vector<int> copyVec;
-  for(int i=0; i<numN; i++){
-    copyVec.push_back(numVec[i]);
+  // 중복제거 및 정렬
+  sort(numVec.begin(), numVec.end());
+  numVec.erase(unique(numVec.begin(),  numVec.end()),numVec.end());  
+
+  // 중복제거하고 정렬하면 index가 압축된 좌표와 같은것을 이용
+  for (int i = 0; i < numN; i++) {
+    int idx = lower_bound(numVec.begin(), numVec.end(),copyArr[i]) - numVec.begin();
+    printf("%d ", idx);
   }
   
-  sort(numVec.begin(),  numVec.end());
-  numVec.erase(unique(numVec.begin(),  numVec.end()),numVec.end());
-
-
-   
-  for(int i=0; i<numN; i++){
-    for(int j=0; j<numVec.size(); j++){
-      if(copyVec[i] == numVec[j]){
-        printf("%d ", j);
-      }
-    }
-  }
+    // 아니 지금까지 시간초과가 출력부에 문제가 있을줄은 몰랐지,,,,
 }
