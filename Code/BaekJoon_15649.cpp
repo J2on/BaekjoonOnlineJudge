88542160bc53348b5d27a4fa4c5e125e23be7368
전재현 <63008127+J2on@users.noreply.github.com>
2022-01-21 23:51:17 +0900

    진짜 모르겠는데

---
+++ Code/BaekJoon_15649.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+/*
+  BaekJoon_15649번 N과M (1)
+*/
+
+int main(){
+  int numN;
+  int numM;
+  scanf("%d %d", &numN, &numM);
+  
+}
