24fa50c5e6371a4970885dcf026258fc64ee67a3
전재현 <63008127+J2on@users.noreply.github.com>
2022-01-22 20:08:32 +0900

    Update BaekJoon_2908.cpp

--- Code/BaekJoon_2908.cpp
+++ Code/BaekJoon_2908.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 using namespace std;
 
+// 정수를 입력받아 뒤집어 주는 함수
+// 10으로 나눈 나머지 구하기 → NUM을 10으로 나누어 주기 를 반복하면 각 자릿수를 구할 수 있다.
 int reverseInt(int num){
   int numList[3];
 
@@ -12,6 +14,9 @@ int reverseInt(int num){
   return result;
 }
 
+// 굳이 함수를 따로 만들어야 했나 싶지만 이때 함수로 나누는 연습을 하고 있던 중이라...
+// 그리고 개인적으로는 MAIN함수를 깔끔하게 가져가는것이 좋은것 같다.
+// 비교해서 큰 수를 리턴하는 함수
 int compareInt(int numA, int numB){
   if (numA > numB) {return numA;}
   else {return numB;}
