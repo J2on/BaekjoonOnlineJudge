281f97b5dc0086f49090120f0691122829d83ef6
전재현 <63008127+J2on@users.noreply.github.com>
2022-01-31 22:47:22 +0900

    이건 그냥 따라했다...

--- Code/BaekJoon_9663.cpp
+++ Code/BaekJoon_9663.cpp
@@ -2,69 +2,41 @@
 #include<vector>
 using namespace std;
 
+/* 
+ BaekJoon_9663 : N-Queen
+ https://chanhuiseok.github.io/posts/baek-1/
+ 참고 했습니다.
+*/
 int numN;
 int result = 0;
-vector<vector<bool>> isOk;
+int arrQueen[15];
 
-void printBool(vector<vector<bool>> boolA){
-  cout << endl;
-  for(vector<bool> bV:boolA){
-    for(bool b:bV){
-      if(b == true){
-        cout << " T    ";
-      }
-      else {
-        cout<< " F    ";
-      }
+bool testLocation(int numQ){
+  for(int i=0; i<numQ; i++){
+    if(arrQueen[i] == arrQueen[numQ] || numQ - i == abs(arrQueen[numQ] - arrQueen[i])){
+      return false;
     }
-    cout << endl;
   }
+  return true;
 }
-
 
 void chess(int numQ){
   if(numQ == numN){
     result++;
     return;
   }
-  
-  int numBig;
-  int numSm;
+
   for(int x=0; x<numN; x++){
-    for(int y=0; y<numN; y++){
-      if(isOk[x][y] == true) continue;
-      // x값이 같거나 y값이 같은 경우 제외
-      for(int i=0; i<numN; i++){
-        isOk[x][i] = true;
-        isOk[i][y] = true;
-      }
-      numBig = x;
-      if(y>x) numBig = y;
-      for(int i=0; i < numN -numBig; i++){
-        isOk[x+i][y+i] = true;
-      }
-      numSm = x;
-      if(x>y) numSm =y;
-      for(int i=0; i<numSm; i++){
-        isOk[x-i][y-i] = true;
-      }
-      printBool(isOk);
-      chess(numQ+1);
+    arrQueen[numQ] = x;
+    if(testLocation(numQ)){
+      chess(numQ+1);  
     }
   }
-  
 }
 
 
 int main() {
   scanf("%d", &numN);
-  vector<bool> emptyVec;
-  for(int x=0; x<numN; x++){
-    isOk.push_back(emptyVec);
-    for(int y=0; y<numN; y++){
-      isOk[x].push_back(false);
-    }
-  }
   chess(0);
   printf("%d", result);
 }
