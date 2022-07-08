01c4e117aaa6cfed9277a378f4e0b1f3218e34bd
전재현 <63008127+J2on@users.noreply.github.com>
2022-07-08 23:42:46 +0900

    Create BaekJoon_25083.cpp

---
+++ V2/BaekJoon_25083.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+  printf("%s", "         ,r\'\"7 \n");
+  printf("%s", "r`-_   ,'  ,/ \n");
+  printf("%s"," \\. \". L_r' \n");
+  printf("%s","   `~\\/ \n");
+  printf("%s","      | \n");
+  printf("%s","      | \n");
+}
